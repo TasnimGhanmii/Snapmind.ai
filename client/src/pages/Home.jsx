39facf579b1a1caabb64f5bfae005effb2911e44
tasnim <tasnimghanmi02@gmail.com>
2025-09-25 16:57:06 +0100

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AITools from '../components/AITools'
+import Testimonial from '../components/Testimonial'
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Hero />
         <AITools/>
+
+        <Testimonial/>
     </div>
   )
 }
